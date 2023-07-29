39ab908660543e960da18d4fe5d1ba582994513e
julioarturo <32986837+julioarturo@users.noreply.github.com>
2023-07-28 18:03:55 -0600

    Test Connection

--- server.js
+++ server.js
@@ -9,9 +9,21 @@
 // console.log("Server on port 3000");
 
 // Server with express
+const { Sequelize } = require("sequelize");
 const express = require("express");
 const app = express();
+const sequelize = new Sequelize("postgres://user:pass@example.com:5432/dbname"); // Example for postgres
 
 app.get("/", (req, res) => res.send("Hello worls with express!"));
+app.get("/test-connection", (req, res) => testConnection(res));
 app.listen(80);
 console.log("Server on port 80 with express");
+
+async function testConnection(res) {
+  try {
+    await sequelize.authenticate();
+    res.send("Connection");
+  } catch (error) {
+    res.send("Error");
+  }
+}
